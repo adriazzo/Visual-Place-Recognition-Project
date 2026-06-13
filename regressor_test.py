8e57868fa4a58a89979c6c185355681d5701f2de
Adriano Azzolini <161461366+adriazzo@users.noreply.github.com>
2026-06-13 20:09:22 +0200

    Update regressor_test.py

--- regressor_test.py
+++ regressor_test.py
@@ -34,8 +34,9 @@ def build_dataset(preds_dir, inliers_dir):
     return np.array(X), np.array(y)
  
 def test_model(X, y, model_path):
-    bundle = joblib.load(model_path)
-    clf = bundle['clf']
+    X = np.asarray(X).reshape(-1,1)
+    y = np.asarray(y).reshape(-1,1)
+    clf = joblib.load(model_path)
 
     probs = clf.predict_proba(X)[:,1]
     preds = clf.predict(X)
@@ -46,27 +47,29 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_base", required = True, help="Cartella col dataset")
     parser.add_argument("--model_dir", required = True, help="Modello trainato")
-    parser.add_argument("--vpr_list", required = True)
-    parser.add_argument("--matchers_list", required = True)
+    parser.add_argument("--vpr_list", required = True, nargs = '+')
+    parser.add_argument("--matchers_list", required = True, nargs = '+')
     return parser.parse_args()
 def main(args):
     test_base = Path(args.test_base)
-    model_path = Path(args.model_path)
-    vpr_list = list(args.vpr_list)
-    matchers_list = list(args.matchers_list)
-    
-    for vpr_dir in test_base.iterdir() if vpr_dir.name in vpr_list else None:
-      vpr_method = vpr_dir.name
+    model_path = Path(args.model_dir)
+    vpr_list = args.vpr_list
+    matchers_list = args.matchers_list
+    for vpr_dir in test_base.iterdir():
+      if vpr_dir.name in vpr_list:
 
-      matcher_dirs = sorted(
-          d for d in vpr_dir.iterdir()
-          if d.is_dir() and d.name.startswith("preds_")
-      )
-      for matcher_dir in sorted(matcher_dirs) if matcher_dir.name.replace("preds_","") in matchers_list else None:
-          matcher = matcher_dir.name.replace("preds_","")
+        vpr_method = vpr_dir.name
 
-          X, y = build_dataset(str(vpr_method / "preds"), str(matcher_dir))
-          test_model(X, y, model_path)
+        matcher_dirs = []
+        for d in vpr_dir.iterdir():
+          if d.name.startswith("preds_"):
+            matcher_dirs.append(d)
+        for matcher_dir in sorted(matcher_dirs):
+          if matcher_dir.name.replace("preds_","") in matchers_list:
+            matcher = matcher_dir.name
+            X, y = build_dataset(str(vpr_dir / 'preds'), str(matcher_dir))
+            print(vpr_method, matcher)
+            test_model(X, y, model_path)
     
 if __name__ == "__main__":
     args = parse_args()
